59df9301d2283821c199fcbb0d8fe257cbb47104
luizalexandrew <luiz.alexandre@live.com>
2016-08-19 16:53:06 -0300

    Learning GO

---
+++ 002 - Variáveis/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var numero int 
+	numero = 25
+	fmt.Println(numero + 15)
+
+	nome := "luizalexandrew"
+	numero = 12
+
+	fmt.Println(nome, numero)
+
+	nome, numero = "Google", 1
+
+	fmt.Println(nome, numero)
+
+	nome2, numero2 := "Microsoft", 2
+
+	fmt.Println(nome2, numero2)
+
+	var final string = "BIIIRRR"
+	fmt.Println(final)
+
+}
+
