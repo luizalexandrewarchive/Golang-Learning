d3bf57a02741db4dba0ecf2f6852c089ca8a572e
luizalexandrew <luiz.alexandre@live.com>
2016-08-20 16:31:19 -0300

    Conversion C to F

---
+++ 003 - Args/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	
+	if len(os.Args) < 3 {
+		fmt.Println("Passe o <valores> e <unidade>")
+		os.Exit(1)
+	}
+
+	unidadeOrigem := os.Args[len(os.Args)-1]
+	valoresOrigem := os.Args[1 : len(os.Args)-1]
+
+	fmt.Println(os.Args[1 : len(os.Args)-1])
+
+	var unidadeDestino string
+
+	if unidadeOrigem == "C" {
+		unidadeDestino = "F"
+	} else if unidadeOrigem == "K" {
+		unidadeDestino = "M"
+	} else {
+		fmt.Printf("%s não é uma unidade conhecida!", unidadeOrigem)
+		os.Exit(1)
+	}
+
+	for i, v := range valoresOrigem {
+		valorOrigem, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			fmt.Printf("O valor %s na posição %d não é um número válido!\n", v, i)
+			os.Exit(1)
+		}
+
+		var valorDestino float64
+
+		if unidadeOrigem == "C" {
+			valorDestino = valorOrigem*1.8 + 32
+		} else {
+			valorDestino = valorOrigem / 1.60934
+		}
+
+		fmt.Printf("%.2f %s = %.2f %s\n",
+			valorOrigem, unidadeOrigem, valorDestino, unidadeDestino)
+	}
+}
