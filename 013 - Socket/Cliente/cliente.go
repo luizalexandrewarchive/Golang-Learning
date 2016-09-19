096f9d1efca89b8ef54a1ddf377c6d86cd173d96
luizalexandrew <luiz.alexandre@live.com>
2016-09-19 02:41:38 -0300

    Socket sell

--- 013 - Socket/Cliente/cliente.go
+++ 013 - Socket/Cliente/cliente.go
@@ -1,9 +1,12 @@
 package main
 
-import "net"
-import "fmt"
-import "bufio"
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+)
 
 func main() {
 
@@ -17,8 +20,28 @@ func main() {
 		fmt.Print("Senha: ")
 		senha, _ := reader.ReadString('\n')
 		fmt.Fprintf(conn, senha+"\n")
-
 		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print(message)
+		if message == "Logado com sucesso\n" {
+			fmt.Println(message)
+			break
+		} else {
+			fmt.Println(message)
+		}
 	}
+	fmt.Println("----------------Buscando Shows-----------------")
+	qtnShow, _ := bufio.NewReader(conn).ReadString('\n')
+
+	qtdShowInt, _ := strconv.Atoi(qtnShow[:len(qtnShow)-1])
+
+	for count := 0; qtdShowInt > count; count++ {
+		show, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Println(show[:len(show)-1])
+
+	}
+	fmt.Println("-----Identifique qual show quer comprar--------")
+	compra, _ := reader.ReadString('\n')
+	fmt.Fprintf(conn, compra+"\n")
+
+	message, _ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println(message)
 }
--- 013 - Socket/Servidor/servidor.go
+++ 013 - Socket/Servidor/servidor.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -21,6 +23,27 @@ func main() {
 		if isValido == true {
 			fmt.Println("Usuário Logado")
 			conn.Write([]byte(string("Logado com sucesso\n")))
+			quantidadeShow := getQuantidadeShow()
+
+			time.Sleep(100 * time.Millisecond)
+			conn.Write([]byte(quantidadeShow + "\n"))
+			time.Sleep(100 * time.Millisecond)
+			conn.Write([]byte("1 - Lambda Lambda Lambda (R$120,00)\n"))
+			time.Sleep(100 * time.Millisecond)
+			conn.Write([]byte("2 - Hora do show pohaa (R$320,00)\n"))
+			time.Sleep(100 * time.Millisecond)
+			conn.Write([]byte("3 - Processinho a galope (R$560,00)\n"))
+			time.Sleep(100 * time.Millisecond)
+			conn.Write([]byte("4 - BIRRRRRRR (R$890,00)\n"))
+			time.Sleep(100 * time.Millisecond)
+
+			compra, _ := bufio.NewReader(conn).ReadString('\n')
+			compraString, _ := strconv.Atoi(compra)
+			fmt.Println("\nCompra realizada pelo usuario: ", email)
+			fmt.Println("ID Show comprado:", compra)
+
+			conn.Write([]byte("Item " + string(compraString) + "Comprado com sucesso\n"))
+
 		} else {
 			conn.Write([]byte(string("Usuário ou senha errada\n")))
 		}
@@ -28,8 +51,8 @@ func main() {
 	}
 }
 
-func listarIngressos() {
-
+func getQuantidadeShow() string {
+	return "4"
 }
 
 func validar(email string, senha string) bool {
